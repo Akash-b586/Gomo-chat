382dee81282767782f8f1e59e86390a5c0f7dbee
Akash-b586 <153656980+Akash-b586@users.noreply.github.com>
2025-02-25 12:28:07 +0530

    fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
-  ? "https://gomo-chat.onrender.com/"
-  : "https://gomo-chat.onrender.com/",                     
+  ? "https://gomo-chat.onrender.com/api"
+  : "https://gomo-chat.onrender.com/api",                     
 
   withCredentials: true,
 });
